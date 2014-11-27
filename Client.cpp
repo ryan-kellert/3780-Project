b766599cc280b53c07e8be6d5af010f39ad7925b
David Renaud <david.renaud@uleth.ca>
2014-11-27 01:34:27 -0700

    Uploaded the correct files this time

--- Client.cpp
+++ Client.cpp
@@ -1,5 +1,3 @@
-#define MAX_BUFFER_SIZE 255
-
 #include <iostream>
 #include <string>
 #include <iomanip>
@@ -11,207 +9,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <string>
 #include <cstring>
 #include <vector>
 #include <limits>
 #include "Packet.h"
 
-using namespace std;
-
-unsigned seq_num = 0;
-vector<Packet> sent_messages;
-vector<Packet> received_messages;
-
 int main(int argc, char *argv[])
 {
+    //Storage for sent messages until they are acknowledged
+    std::vector<Packet> sent_messages;
 
-    if (argc != 3)
+
+    // Should have the IP address of the server to connect to and the chosen
+    // 5 character client name provided on the command line as program arguments.
+    if (argc != 3 || strlen(argv[2]) != 5)
     {
-        printf("Server address and/or client name not given.\n");
+        std::cout << "Usage: Client server_address 5_character_client_name\n";
         exit(1);
     }
 
 
-    char buffer[MAX_BUFFER_SIZE + 1];
-
-    int socket_descriptor;
-    socket_descriptor= socket(AF_INET, SOCK_DGRAM, 0);
+    //Creating the socket. Program exits if an error occurs.
+    int socket_descriptor = socket(AF_INET, SOCK_DGRAM, 0);
     if (socket_descriptor < 0)
     {
-        cerr<< "Error creating the socket." <<endl;
-        return 0;
-    }
-
-    int port_number=3500;
-    unsigned int server_length= sizeof(struct sockaddr_in);
-    struct sockaddr_in server_address;
-    struct hostent *host_ptr;
-
-    host_ptr= gethostbyname(argv[1]); //IP changes with machine.
-    if(host_ptr==0)
-    {
-        cerr<< "Wrong server address"<<endl;
+        std::cerr << "Error creating the socket.\n";
         exit(1);
     }
+
+    //Set up server connection stuff.
+    struct sockaddr_in server_address;
+    struct hostent* host_ptr = gethostbyname(argv[1]);
+    if(host_ptr == 0)
+    {
+        std::cerr << "Could not establish connection to server.\n";
+    }
     server_address.sin_family = AF_INET;
+    unsigned port_number = 3500;
     server_address.sin_port = htons(port_number);
-    // Make sure we understand what bcopy is doing.
     bcopy((char *)host_ptr->h_addr,
           (char *)&server_address.sin_addr, host_ptr->h_length);
 
-    // Why don't we need server_address.sin_addr.s_addr=htonl(INADDR_ANY);
 
+    //Main Program loop.
+    char action = 'Z';
+    unsigned last_seq_num = 0; //Stores the sequence number last used.
 
-    int receive_error_check;
-    int send_error_check;
-
-    //struct sockaddr_int client_address;
-
-    while(1)
+    std::cout << "Awesome Networking program:\n\n\n";
+    do
     {
-        // user inputs their message, the buffer sets to 000000
-        bzero(buffer, 256);
-        //could use fgets(buffer, 255, stdin). 255 + null terminator
-        // setw limits the users input to 255 characters MAX.
-        // cin>> setw(255)>> buffer;
+        std::cout << "S: Send a message.\n"
+                  << "G: Retrieve messages from the server.\n"
+                  << "Q: Quit the program.\n"
+                  << "\nChoose an action: ";
+        std::cin >> action;
+        action = toupper(action);
 
-        //only sending buffer to the first space of the message.
-        char action;
-
-        cout << "Awesome Networking program:\n\n\n"
-             << "S: Send a message.\n"
-             << "G: Retrieve messages from the server.\n"
-             << "Q: Quit the program.\n"
-             << "\nChoose an action: ";
-
-        cin >> action;
-        switch (toupper(action))
+        switch(action)
         {
+        //Send a message
         case 'S':
-        {
-            std::string message;
-            std::string client_name;
-            std::cout << "\n\nEnter a message: ";
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::getline(std::cin, message);
-            std::cout << "\nWho do you want to send to? ";
-            std::cin >> client_name;
-            Packet packet_to_send(++seq_num, 'S', argv[2], client_name, message);
-            strcpy(buffer, packet_to_send.GetPacketString());
-
-            //Send packet
-            send_error_check= sendto( socket_descriptor, buffer, MAX_BUFFER_SIZE, 0,
-                                      (struct sockaddr *)&server_address,
-                                      server_length);
-            if (send_error_check < 0)
             {
-                cerr << "Error sending." << endl;
-            }
-            else
-            {
-                cout << "Message was sent..." << endl;
-                sent_messages.push_back(packet_to_send);
-            }
-        }
-        break;
-        case 'G':
-        {
-            static unsigned last_seq_num;
-            Packet packet_to_send(++seq_num, 'G', argv[2], "     ", " ");
-            strcpy(buffer, packet_to_send.GetPacketString());
-
-            //Send packet
-            send_error_check= sendto( socket_descriptor, buffer, MAX_BUFFER_SIZE, 0,
-                                      (struct sockaddr *)&server_address,
-                                      server_length);
-
-            if (send_error_check < 0)
-            {
-                cerr << "Error sending request." << endl;
-            }
-            else
-            {
-                Packet* current_packet;
-                cout << "Get in Progress" << endl;
-                do
+                std::string message;
+                std::string client_name;
+                std::cout << "\n\nEnter a message: ";
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::getline(std::cin, message);
+                std::cout << "\nWho do you want to send to? ";
+                std::cin >> client_name;
+                Packet message_packet(++last_seq_num, 'S', argv[2], client_name, message);
+                bool sent = message_packet.Send(socket_descriptor, server_address);
+                if(sent)
                 {
-                    bzero(buffer, MAX_BUFFER_SIZE);
-                    receive_error_check = recvfrom( socket_descriptor, buffer, 256, 0,
-                                                    (struct sockaddr *)&server_address,
-                                                    &server_length);
+                    sent_messages.push_back(message_packet);
+                }
+                else
+                {
+                    std::cerr << "An error occurred while attempting to send this message.\n";
+                }
+            }
+            break;
+        //Get messages from server
+        case 'G':
+            {
+                Packet get_messages(++last_seq_num, 'G', argv[2], "servr", "Gimme my messages!");
+                get_messages.Send(socket_descriptor, server_address);
 
-                    if (receive_error_check < 0)
+                while(true)
+                {
+                    Packet received_packet = Packet::Receive(socket_descriptor, server_address);
+                    if(received_packet.GetMessageType() == 'D')
                     {
-                        cerr<< "Error receiving."<<endl;
+                        //all messages sent by server
+                        std::cout << "End of Messages.\n\n";
+                        break;
+                    }
+                    else if(received_packet.GetMessageType() == 'A')
+                    {
+                        std::cout << "Acknowledgement received for sequence number " << received_packet.GetMessage() << std::endl;
+                        //ummm...do something with an ACK?
                     }
                     else
                     {
-                        Packet packet_received(buffer);
-                        received_messages.push_back(packet_received);
-
-                        for(unsigned i = 0; i < packet_received.size(); i++)
-                        {
-                            if(packet_received.GetSequenceNumber() > last_seq_num);
-                            {
-                                cout << packet_received.GetMessage() << endl;
-                                break;
-                            }
-                            if(received_messages[i].GetSequenceNumber() == packet_received.GetSequenceNumber())
-                            {
-
-                            }
-                        }
-                        cout << "The following message was received: " << endl;
-
-                        Packet ACK(++seq_num, 'A', argv[2], packet_received.GetSourceName(), itoa(packet_received.GetSequenceNumber());
-                        current_packet = &packet_received;
-                        send_error_check= sendto( socket_descriptor, ACK.GetPacketString(), MAX_BUFFER_SIZE, 0,
-                                      (struct sockaddr *)&server_address,
-                                      server_length);
+                        Packet ACK(++last_seq_num, 'A', argv[2],
+                                   received_packet.GetSourceName(),
+                                   std::to_string(received_packet.GetSequenceNumber()));
+                        ACK.Send(socket_descriptor, server_address);
+                        std::cout << received_packet.GetMessage() << std::endl;
                     }
-
-                }while(current_packet->GetMessageType() != 'D');
+                }
             }
+            break;
+        //Quit the program
+        case 'Q':
+            break;
+        default:
+            std::cout << "Invalid selection made. Try again\n"
+            << "Press any key to continue..." << std::endl;
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cin.get();
+            break;
         }
-        break;
-        }
-
-
-
-        // We put server address in both because there will only be client to
-        // server communication.
-        /*Packet packet_to_send(++seq_num, )
-
-        for(unsigned i = 0; i < send_string.size(); i++)
-        {
-            buffer[i] = send_string[i];
-        }
-
-
-        // Only for testing, remove once complete.
-
-
-        receive_error_check = recvfrom( socket_descriptor, buffer, 256, 0,
-                                        (struct sockaddr *)&server_address,
-                                        &server_length);
-
-        if (receive_error_check<0)
-        {
-            cerr<< "Error receiving."<<endl;
-            // Only for testing, remove once complete.
-            cout<<"The following message was received: "<<endl;
-            for (unsigned i = 26; i < MAX_BUFFER_SIZE; i++)
-            {
-                cout<<buffer[i];
-            }
-            cout << endl;
-        }
-        // Upon receiving message from the server. Send another message with
-        // acknowledgement.*/
     }
-
-    close(socket_descriptor);
-    return 0;
+    while(action != 'Q');
 }
-
--- Server.cpp
+++ Server.cpp
@@ -8,195 +8,74 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <vector>
+#include <map>
 #include "Packet.h"
-using namespace std;
 
-
-#define MAX_BUFFER_SIZE 255
-
-
-int main()
+int main(int argc, char *argv[])
 {
+    //Storage for messages until clients request them.
+    std::map<std::string,std::vector<Packet>> client_messages;
 
-   // We will store our messages here.
-   vector<Packet> Message_Storage;
-   // The buffer will hold the messages on the server side.
-   // Any messages above the size of 256 will be incomplete
-   // and will therefore produce an error message. 
-   char buffer[MAX_BUFFER_SIZE +1 ];
+    //Creating the socket. Program exits if an error occurs.
+    int socket_descriptor = socket(AF_INET, SOCK_DGRAM, 0);
+    if (socket_descriptor < 0)
+    {
+        std::cerr << "Error creating the socket.\n";
+        exit(1);
+    }
 
+    //Set up server connection stuff.
+    struct sockaddr_in server_address;
+    server_address.sin_family= AF_INET;
+    unsigned port_number = 3500;
+    server_address.sin_port= htons(port_number);
+    server_address.sin_addr.s_addr=htonl(INADDR_ANY);
+    int bind_error = bind(socket_descriptor, (struct sockaddr *)&server_address, sizeof(server_address));
+    if(bind_error)
+    {
+        std::cerr << "Server cannot bind socket on port " << port_number << std::endl;
+        exit(1);
+    }
 
-   int socket_descriptor;
-   // The socket descriptor will be the return value, which
-   // will be used in later system calls. 
-   //
-   // \param AF_INENET = Domain IPv4
-   // \param SOCK_DGRAM = Type Datagram
-   // \param 0 = Protocol UDP
-   socket_descriptor= socket(AF_INET, SOCK_DGRAM, 0);
-   if (socket_descriptor < 0)
-   {
-     //printf("Error opening the socket.");
-      cerr<< "Error creating the socket."<<endl;
-      exit(1);
-   }
-   
-   // Socket Address struct. 
-   int port_number= 3500; 
-   struct sockaddr_in server_address;
-   server_address.sin_family= AF_INET; // Domain IPv4 protocol
-   server_address.sin_port= htons(port_number); // htons?
-   // INADDR_ANY binds the socket to the local IP address
-   server_address.sin_addr.s_addr=htonl(INADDR_ANY); // should this be htons or htonl?
+    struct sockaddr_in client_address;
+    unsigned last_seq_num = 0;
 
-  
-   int server_length= sizeof(server_address);
-   // Now that we have a socket, we have to associate this socket with a port on the
-   // machine we are using.
-   //
-   // \param socket_descriptor = our socket file descriptor.
-   // \param server_address = pointer to struct sockaddr that contains info about
-   // the servers address: Port and IP address.
-   // \param server_length is the length in bytes of the previous parameters address. 
-   int bindCheck;
-   bindCheck=bind(socket_descriptor, (struct sockaddr *)&server_address,server_length);
-   if (bindCheck<0)
-   {
-      cerr<<"Binding error."<<endl;
-      exit(1);
-   }
-
-   // Checks to see if recvfrom returns error -1.
-   int receive_error_check;
-   // Checks to see if sendto returns error -1.
-   int send_error_check;
-   struct sockaddr_in client_address;
-   socklen_t client_length= sizeof(struct sockaddr_in);
-   
-   while(1)
-   {
-      // Receives a message from a client. Stores the message in
-      // a buffer, and gives the client_address an IP address, port
-      // and length. 
-      //
-      // \param socket_descriptor
-      // \param buffer: the buffer that will hold the received message
-      // \param strlen(buffer): the size of our buffer. Max sz of msg.
-      // \param 0: Flags
-      // \param client_address: A pointer to a yet to be initialized sockaddr
-      // recvfrom fills in the client's IP address and port, to be used in
-      // send to.
-      // \param client_length: The length of the client address.
-      cout<< "Waiting for messages... " << endl;
-         
-      receive_error_check= recvfrom( socket_descriptor,buffer,MAX_BUFFER_SIZE, 0,
-				     (struct sockaddr *)&client_address,
-				     &client_length);
-      if(receive_error_check < 0)
-      {
-	 cerr<<"Failed to receive."<< buffer <<  endl;
-      }
-
-      Packet packet_received(buffer);
-      packet_received.GetMessage();
-      cout<< packet_received.GetMessage()<<endl;
-         
-      
-      
-      
-    
-      
-      Message_Storage.push_back(packet_received);
-      switch ( packet_received.GetMessageType() )
-      {
-	 case 'G':
-	 {
-	    unsigned seq_no = 0;
-	    for(unsigned i=0; i<Message_Storage.size(); i++)
-	    {
-	       if( Message_Storage[i].GetDestinationName()==packet_received.GetSourceName())
-	       {
-		  send_error_check= sendto( socket_descriptor,
-					    Message_Storage[i].GetPacketString(),
-					    strlen(Message_Storage[i].GetPacketString()), 0,
-					    (struct sockaddr *)&client_address,
-					    client_length);
-		  
-		  
-		  if(send_error_check < 0)
-		  {
-		     cerr<< "Problems sending."<< endl;
-		     break;
-		  }
-		  seq_no++;
-	       }
-	       
-	    }
-	    Packet end_of_messages(seq_no, 'D', "", "", "All Messages Received.\n" );
-	    send_error_check= sendto( socket_descriptor,
-				      end_of_messages.GetPacketString(),
-				      strlen(end_of_messages.GetPacketString()), 0,
-				      (struct sockaddr *)&client_address,
-				      client_length);
-		  
-	    
-	    if(send_error_check < 0)
-	    {
-	       cerr<< "Problems sending."<< endl;
-	       break;
-	    }
-	    break;
-	 }
-	 
-	 case 'S':
-	    break;
-	    
-	 case 'A':
-	    break;
-	 default:
-	    cerr<<"Type not valid."<<endl;
-	    break;
-      }
-      
-
-      // Datagram sockets are not connected to a remote host, so we just need to
-      // send the destination address with our packet.
-      //
-      // \param socket_descriptor
-      // \param buffer: the buffer holds our message, so we are passing in
-      // the buffer to send our message.
-      // \param strlen(buffer) informs the function that our message size
-      // is the size of the buffer. For simplicity we will just send the
-      // entire buffer for every message.
-      // \param 0: Flags
-      // \param client_address: pointer to the client's address which
-      // contains the clients IP address and port. This destination
-      // address is determined from the recvfrom() function prior. 
-      // \param client_length: is the size of the previous parameter. 
-      // send_error_check= sendto( socket_descriptor, buffer,strlen(buffer), 0,
-      //			(struct sockaddr *)&client_address,
-      //			 client_length);
-      // if(send_error_check < 0)
-      // {
-//	  cerr<< "Problems sending."<< endl;
-      //      }
-       
-
-   }
-     
-   
-   
-
-      
-      
-   return 0;
-
-   
+    while(true)
+    {
+        std::cout << "Waiting for Messages\n";
+        Packet received_message = Packet::Receive(socket_descriptor, client_address);
+        std::cout << "Message Received: " <<  received_message.GetMessage() << std::endl;
+        switch (received_message.GetMessageType())
+        {
+        case 'A':
+        {
+            unsigned ACK_seq_num = std::stoul(received_message.GetMessage());
+            for(unsigned i = 0; i < client_messages[received_message.GetSourceName()].size(); i++)
+            {
+                if(client_messages[received_message.GetSourceName()][i].GetSequenceNumber() == ACK_seq_num)
+                {
+                    client_messages[received_message.GetSourceName()].erase(client_messages[received_message.GetSourceName()].begin()+i);
+                }
+            }
+            client_messages[received_message.GetDestinationName()].push_back(received_message);
+        }
+        break;
+        case 'S':
+            client_messages[received_message.GetDestinationName()].push_back(received_message);
+            break;
+        case 'G':
+        {
+            for(unsigned i = 0; i < client_messages[received_message.GetSourceName()].size(); i++)
+            {
+                client_messages[received_message.GetSourceName()][i].Send(socket_descriptor, received_message.from_address);
+            }
+            Packet end_of_messages(++last_seq_num, 'D', "servr", received_message.GetSourceName(), "All messages sent");
+            end_of_messages.Send(socket_descriptor, received_message.from_address);
+        }
+        break;
+        default:
+            std::cerr << "Invalid packet received.";
+            break;
+        }
+    }
 }
-
-   
-
-   
-
-
-    
